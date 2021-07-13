ad45715d14e1c11bce34bb57a9f9744cedd650f0
Márcio Ribeiro <marciosribeiro01@outlook.com>
2021-07-13 18:14:21 -0300

    consumo-2

--- blogDoMarcio/src/app/entrar/entrar.component.ts
+++ blogDoMarcio/src/app/entrar/entrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserLogin } from '../model/userLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entrar',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entrar.component.css']
 })
 export class EntrarComponent implements OnInit {
+  
+  userLogin: UserLogin = new UserLogin
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+  entrar(){
+    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin) =>{
+      this.userLogin = resp
+      this.router.navigate(['/incio'])
+    }, erro =>{
+      if(erro.status == 500){
+        alert('Usuário ou senha incorretos')
+      }
+    }
+    )
+
   }
 
 }
